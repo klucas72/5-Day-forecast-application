59fadb5bf61ad3897615552676c23b80a2a80b42
klucas72 <klucas72@hotmail.com>
2021-03-21 18:02:17 -0400

    added 5 day forecast details to cards

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,8 @@ var currentDay = document.getElementById('current-day')
 var currentTemp = document.getElementById('current-temp')
 var currentUvi = document.getElementById('current-uvi');
 var currentIcon = document.getElementById('current-icon');
+var forecastSection = document.getElementById('forecast-section')
+
 
 function getApi() {
 
@@ -29,7 +31,7 @@ function getApi() {
         var latitude = data.coord.lat;
         var longitude = data.coord.lon;
 
-        var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=029bba2a4f5f9352670594571d57d373`
+        var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=minutely,hourly&appid=029bba2a4f5f9352670594571d57d373`
         console.log(requestUrl)
         fetch(requestUrl).then(function (response2) {
             return response2.json()
@@ -42,16 +44,24 @@ function getApi() {
             iconSlug = current.weather[0].icon;
             currentIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${iconSlug}.png"/><div>${current.weather[0].description}</div>`
 
-            for (var i = 0; i < data2.daily.length; i++) {
-                eachDay = data2.daily[i];
-                console.log('this is the daily high ' + eachDay.temp.day);
-                console.log('this is the daily low ' + eachDay.temp.night);
-                console.log('this is the daily UV index ' + eachDay.uvi);
+            for (var i = 1; i < data2.daily.length - 2; i++) {
+                var eachDay = data2.daily[i];
+                var eachDate = new Date(eachDay.dt * 1000)
+                var formattedDate = eachDate.toLocaleDateString('en-US', { weekday: 'long' });
+                var newForecast = document.createElement('div');
+                newForecast.innerHTML = `<div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">${formattedDate}</h5>
+                  <p class="card-text">humidity: ${eachDay.humidity}%</p>
+                  <p class="card-text">wind speed: ${Math.round(eachDay.wind_speed)} mph</p>
+                  <p class="card-text">Temperature: ${Math.round(eachDay.temp.day)} °F</p>
+                </div>
+              </div>`
+                forecastSection.appendChild(newForecast)
+                // console.log('this is the daily high ' + eachDay.temp.day);
+                // console.log('this is the daily low ' + eachDay.temp.night);
+                // console.log('this is the daily UV index ' + eachDay.uvi);
             }
-            var weatherData1 = eachDay.temp.day;
-            var weatherData2 = eachDay.temp.night;
-            var weatherdata3 = eachDay.uvi;
-
         });
     });
 
